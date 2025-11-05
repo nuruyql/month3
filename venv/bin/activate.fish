912c59d7abeb826446cee16edd177ca5c0064673
nuruyql <nurbolsuiunbekov626@gmail.com>
2025-11-05 12:36:08 +0600

    prepared the files,packages

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/amature/my_flet_app/venv
+set -gx VIRTUAL_ENV /home/amature/Desktop/my_flet_app/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
